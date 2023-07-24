4d5c6c04ae3a04d40e3dbdf2d9b633e3bb509000
Aaron <github@hoffer.org>
2023-07-24 16:01:29 -0700

    Last commit before stopping work

--- src/main/java/com/example/pmddemo/HotTopic.java
+++ src/main/java/com/example/pmddemo/HotTopic.java
@@ -1,46 +1,24 @@
 package com.example.pmddemo;
 
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Component;
 
-@Component
+import org.springframework.jms.core.JmsTemplate;
+
+import java.util.concurrent.Callable;
+
 public class HotTopic {
 
-    final ConnectionFactory connectionFactory;
-    final RabbitTemplate template;
-    final String address = "hot.topic";
-    Object lastIheard;
+    Callable<Object> x;
+    JmsTemplate template;
 
-    public HotTopic(ConnectionFactory connectionFactory) {
-        this.connectionFactory = connectionFactory;
-        this.template = new RabbitTemplate(connectionFactory);
-    }
+    public void foobar(String message) throws Exception {
 
-    public void send(String message) {
-        template.convertAndSend(address, message);
-    }
+        x = () -> 66;
 
+        template.convertAndSend(
 
-    // LISTENER IS NOT WORKING IN THE TESTS
-    // Throws an exception when receive() is called if the address is not defined in the broker
-    @RabbitListener(queues = "hot.topic")
-    void receive(Message message) {
-        lastIheard = message.getBody();
-    }
+                "hot.topic",
 
-    Object receiveAndConvert() {
-        lastIheard = template.receiveAndConvert(address);
-        return lastIheard;
-    }
-
-    Object receive() {
-        Message message = template.receive(address);
-        if (message != null)
-            lastIheard = message.getBody();
-        return lastIheard;
+                x.call());
     }
 }
 
